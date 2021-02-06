c2cd5d93e7771291964ba74d37ca50332d9f6018
Dimitris Frangiadakis <itp20159@hua.gr>
2021-02-06 21:20:12 +0200

    CM works

--- common_neighbors.py
+++ common_neighbors.py
@@ -4,8 +4,8 @@ from pyspark import SparkContext
 
 
 def main(args):
-    # def debug(x):
-    #     print(x)
+    def debug(x):
+        print(x)
 
     sc = SparkContext(appName='common_neighbors', master=args.master)
 
@@ -21,23 +21,41 @@ def main(args):
         s, t = edge
         return [edge, (t, s)]
 
-    edges = edges.flatMap(edge_to_edge_plus_reversed)
+    edges = edges.flatMap(edge_to_edge_plus_reversed).distinct()
 
-    joined_edges = edges.join(edges)
+    joined_edges = edges.join(edges).distinct()
 
     def pairs_with_common_neighbors(pair):
         source = pair[1][0]
         target = pair[1][1]
         if source < target:
-            return [(source, 1)]
+            return [((source, target), 1)]
         else:
             return []
 
     pairs_with_common_neighbors = joined_edges.flatMap(pairs_with_common_neighbors)
+
     reduced_pairs = pairs_with_common_neighbors.reduceByKey(lambda a, b: a + b)
 
-    result = sc.parallelize(reduced_pairs.take(args.limit))
+    def test(t):
+        node = t[0]
+        akmi = t[1][0]
+        haveNeighbor = t[1][1][0]
+        haveNeighborReversed = (haveNeighbor[1], haveNeighbor[0])
+        if akmi != haveNeighbor and akmi != haveNeighborReversed:
+            return True
+        return False
+
+    fuckedEdges = edges.map(lambda edge: (edge, edge))
+    # result = edges.map(lambda edge: (edge[0], edge)).leftOuterJoin()join(reduced_pairs.map(lambda e: (e[0][0], e))).filter(test).map(lambda element: element[1][1]).distinct()
+    result = reduced_pairs.leftOuterJoin(fuckedEdges).distinct().filter(lambda e: e[1][1] is None).map(lambda makrinar: (makrinar[0], makrinar[1][0])).sortBy(lambda a, ascending=False: a[1])
+    # result.foreach(debug)
+    # # reduced_pairs.foreach(debug)
+
+    # result = sc.parallelize(reduced_pairs.take(args.limit))
+    # result.saveAsTextFile(args.output)
     result.saveAsTextFile(args.output)
+    # reduced_pairs.foreach(debug)
     sc.stop()
 
 
