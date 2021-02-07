7ec4a07d2802f16ef1d81c416bcb71ba0a666c22
Dimitris Frangiadakis <itp20159@hua.gr>
2021-02-07 03:14:07 +0200

    JC Refactor

--- jaccard_coefficient.py
+++ jaccard_coefficient.py
@@ -39,50 +39,50 @@ def main(args):
         .flatMap(pairs_with_common_neighbors)\
         .reduceByKey(lambda a, b: a + b)
 
-    result = reduced_pairs_with_common_neighbors\
+    cm = reduced_pairs_with_common_neighbors\
         .leftOuterJoin(edges.map(lambda edge: (edge, edge)))\
         .filter(lambda e: e[1][1] is None)\
         .map(lambda node_object: (int(node_object[1][0]), node_object[0]))\
         .sortByKey(ascending=False)
 
-    def res_to_res_plus_reversed(res):
-        neighbors = res[0]
-        s, t = res[1]
-        return [res, (neighbors, (t, s))]
+    def cm_to_cm_plus_reversed(obj):
+        neighbor_pair = obj[0]
+        s, t = obj[1]
+        return [obj, (neighbor_pair, (t, s))]
 
-    joined_results = result.flatMap(res_to_res_plus_reversed)
+    joined_results = cm.flatMap(cm_to_cm_plus_reversed)
 
-    def fix_keys(key):
-        source = key[0][0]
-        target = key[0][1]
-        count = key[1]
-        neighbor = key[2]
+    def fix_keys(obj):
+        source = obj[0]
+        target = obj[1][0][0]
+        count = obj[1][0][1]
+        neighbor = obj[1][1]
+
         if int(source) > int(target):
             return (target, source), count, neighbor
-        return key
+        return (source, target), count, neighbor
 
-    def finalMap(key):
-        source = key[0][0]
-        target = key[0][1]
-        common_neighbors = key[0][2]
-        all_neighbors = key[1]
+    def calculate_jc(obj):
+        source = obj[0][0]
+        target = obj[0][1]
+        common_neighbors = obj[0][2]
+        all_neighbors = obj[1]
 
         return float(common_neighbors)/float(all_neighbors), (source, target)
 
     neighbors = joined_results\
         .map(lambda obj: (obj[1][0], (obj[1][1], obj[0])))\
         .join(edges)\
-        .map(lambda res: ((res[0], res[1][0][0]), res[1][0][1], res[1][1]))\
         .map(fix_keys)\
         .distinct()\
         .map(lambda obj: ((obj[0][0], obj[0][1], obj[1]), 1))\
         .reduceByKey(lambda a, b: a + b)\
-        .map(finalMap)\
+        .map(calculate_jc)\
         .sortByKey(ascending=False)
 
-    neighbors.foreach(debug)
+    # neighbors.foreach(debug)
     neighbors = sc.parallelize(neighbors.take(args.limit))
-    # neighbors.saveAsTextFile(args.output)
+    neighbors.saveAsTextFile(args.output)
     sc.stop()
 
 
